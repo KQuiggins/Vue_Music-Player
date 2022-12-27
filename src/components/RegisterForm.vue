e95310daae22aab912ae99ba7e351aa6d2467536
KQuiggins <kashfamilyvalues@gmail.com>
2022-12-27 08:13:53 -0500

    connected user data to account with uid

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -149,7 +149,6 @@ export default {
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_msg = "Please wait! Your account is being created.";
 
-
       try {
         await this.createUser(values);
       } catch (error) {
@@ -161,7 +160,6 @@ export default {
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Your account has been created successfully.";
-
     },
   },
 };
