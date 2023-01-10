a85e04838044f973befe7d230b6fcb5602da6ff9
KQuiggins <kashfamilyvalues@gmail.com>
2023-01-09 20:46:48 -0500

    added user logout

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -77,6 +77,7 @@ export default {
       this.login_alert_variant = "bg-green-500";
       this.login_alert_msg = "You have been logged in successfully.";
       console.log(values);
+      window.location.reload();
     },
   },
 };
