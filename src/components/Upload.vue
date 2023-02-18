6f09c6ca7769930db4fd2bde379c004b8261694f
KQuiggins <kashfamilyvalues@gmail.com>
2023-02-18 07:04:46 -0500

    upload ui changes color depending upon success

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -79,12 +79,26 @@ export default {
             text_class: "",
           }) - 1;
 
-        task.on("state_changed", (snapshop) => {
-          const progress =
-            (snapshop.bytesTransferred / snapshop.totalBytes) * 100;
-          console.log("Uploading...");
-          this.uploads[uploadIndex].current_progress = progress;
-        });
+        task.on(
+          "state_changed",
+          (snapshop) => {
+            const progress =
+              (snapshop.bytesTransferred / snapshop.totalBytes) * 100;
+            console.log("Uploading...");
+            this.uploads[uploadIndex].current_progress = progress;
+          },
+          (error) => {
+            this.uploads[uploadIndex].variant = "bg-red-400";
+            this.uploads[uploadIndex].icon = "fas fa-times";
+            this.uploads[uploadIndex].text_class = "text-red-400";
+            console.log(error);
+          },
+          () => {
+            this.uploads[uploadIndex].variant = "bg-green-400";
+            this.uploads[uploadIndex].icon = "fas fa-check";
+            this.uploads[uploadIndex].text_class = "text-green-400";
+          }
+        );
       });
     },
   },
